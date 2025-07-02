32e09322f7f744549d7f729c9a381990f0d50cf9
sirpent5 <tessatonep@gmail.com>
2025-07-02 14:04:44 -0400

    Working more

---
+++ N_QubitsTwoReserviors/exactDiagMethods.py
@@ -0,0 +1,134 @@
+from imports import *
+from defs import numberop, Sigma_minus, Sigma_plus, Sigma_x, Sigma_y, Sigma_z
+from globalMethods import verify_density_matrix
+
+def Liouvillian(H, Ls, hbar = 1):
+    d = len(H) # dimension of the system
+    superH = -1j/hbar * ( np.kron(np.eye(d),H)-np.kron(H.T,np.eye(d)) ) # Hamiltonian part
+    superL = sum([np.kron(L.conjugate(),L) 
+                  - 1/2 * ( np.kron(np.eye(d),L.conjugate().T.dot(L)) +
+                            np.kron(L.T.dot(L.conjugate()),np.eye(d)) 
+                          ) for L in Ls])
+    return superH + superL
+
+def Enlarge_Matrix_site_j(j, N, matrix):
+    # I⊗...⊗I⊗M⊗I...⊗I: Convert local operators into global operators.
+    # j: site, starts in 0.
+    
+    M = np.eye(len(matrix))
+    if j == 0: M = matrix
+    
+    for i in range(1,N):
+        if i == j: M = np.kron(M, matrix)
+        else: M = np.kron(M, np.eye(len(matrix)))        
+
+    return M
+
+def Correlation_Matrix_i_Matrix_j(i,j,N,matrix_i, matrix_j):
+    # I⊗...⊗I⊗M⊗I...⊗I⊗M⊗I⊗I...⊗I
+
+    M = np.eye(len(matrix_i))
+    
+    if j == 0: M = matrix_j
+    elif i == 0: M = matrix_i
+    
+    for k in range(1,N):
+        if k == j: M = np.kron(M, matrix_j)
+        elif k == i: M = np.kron(M, matrix_i)
+        else: M = np.kron(M, np.eye(len(matrix_i)))        
+
+    return M
+
+def S_Term(N, cte_list, SigmaMatrix):
+    # I⊗...⊗I⊗ΔSigma⊗Sigma⊗I...⊗I: Sigma can be Sigmax, Sigmay or Sigmaz.
+    # cte_list = [cte_L, cte_M, cte_R], can be J or ∆.
+
+    Matrix_Sigma = np.zeros((2**N, 2**N))
+
+    cte_L = cte_list[0]
+    cte_M = cte_list[1]
+    cte_R = cte_list[2]    
+    
+    for i in range(0,N-1):
+        M = np.eye(len(SigmaMatrix))
+        
+        if i == 0: M = SigmaMatrix
+       
+        for j in range(1,N):
+            if j == i or j == i + 1: M = np.kron(M, SigmaMatrix)
+            else: M = np.kron(M, np.eye(len(SigmaMatrix)))        
+
+        if i < N/2 - 1: cte = cte_L
+        elif i > N/2 - 1: cte = cte_R
+        else: cte = cte_M
+
+        Matrix_Sigma = Matrix_Sigma + M*cte #cte can be ∆_i or J_i
+
+    return Matrix_Sigma #∑ I⊗...⊗I⊗ΔSigma⊗Sigma⊗I...⊗I
+
+
+def perform_exact_diag(gamma, F_L,F_R, dt, nt, initial_state, H,N):
+
+    
+    L_K = []
+    
+    L_K.append(np.sqrt(gamma*(1-F_L))*Enlarge_Matrix_site_j(0, N, Sigma_minus))  
+    L_K.append(np.sqrt(gamma*F_L)*Enlarge_Matrix_site_j(0, N, Sigma_plus)) 
+    L_K.append(np.sqrt(gamma *(1-F_R))*Enlarge_Matrix_site_j(N-1, N, Sigma_minus))
+    L_K.append(np.sqrt(gamma * F_R)*Enlarge_Matrix_site_j(N-1, N, Sigma_plus))
+     
+    Superoperator = Liouvillian(H, L_K)
+
+    null = null_space(Superoperator)
+    NULL = null[:, 0]
+    rho_ss = NULL.reshape(2**N, 2**N)
+    rho_ss = rho_ss / np.trace(rho_ss)
+
+    referenceN = np.trace(numberop @ rho_ss)
+    print(f"Reference number operator expectation value: {referenceN}")
+
+    # verify_density_matrix(rho_ss)
+    verify_density_matrix(initial_state)
+
+    # Create time evolution operator
+    U = scipy.linalg.expm(Superoperator * dt)
+    rho_t = initial_state.reshape(4,1)  # Vectorized  state
+
+    expectation_value_history = [np.trace(numberop @ initial_state) / np.trace(initial_state)]
+    print("Initial expectation value of number operator:", expectation_value_history[0])
+    time_points = [0]
+
+    # Time evolution loop
+    for step in range(1,nt+1):
+        rho_t = U @ rho_t
+        rho_matrix = rho_t.reshape(2 ,2)
+        rho_matrix = rho_matrix / np.trace(rho_matrix)
+        expectation_value_history.append(np.trace(numberop @ rho_matrix))
+        time_points.append(step * dt)
+    return expectation_value_history, time_points, referenceN
+
+def build_exact_diag_hamiltonian(eps):
+    H = eps*Sigma_minus@Sigma_plus
+    return H
+
+
+def output_exact_diag_results(exact_diag_results, time, nt, eps, mu_L,mu_R,T_L, T_R, time_points, steadyState):
+
+    plt.figure(figsize=(10, 6))
+    time_axis = np.linspace(0, time, nt+1)
+    mu_effective = (mu_L + mu_R) / 2
+    T_effective = (T_L + T_R) / 2
+    time_axis = np.linspace(0, time, nt+1)
+    plt.plot(time_axis, [steadyState] * (nt + 1),
+             label=f'Steady State ($\\langle n \\rangle$ = {steadyState:.4f})',
+             linestyle='--', color='red')
+
+    plt.plot(time_points, exact_diag_results, label='Expectation Value (Simulated)', marker='', linestyle='solid')
+
+    plt.title("Exact Diagonalization Results of two reserviors coupled to a single qubit")
+    plt.xlabel("Time (t)")
+    plt.ylabel("⟨n⟩ (Expectation Value)")
+    plt.grid(True)
+    plt.legend()
+    
+    plt.show()
---
+++ N_QubitsTwoReserviors/vqteMethods.py
@@ -0,0 +1,80 @@
+
+# from qiskit.quantum_info import SparsePauliOp
+# import numpy as np
+from imports import *
+def hamiltonian_generation(eps, gamma, F_R,F_L):
+    """
+    Generates the Hamiltonian for the system of a single qubit coupled to a reservoir.
+    
+    Inputs:
+        eps: (float) Coupling strength of the qubit to the reservoir.
+        gamma: (float) Coupling strength of the qubit to the environment.
+        mu: (float) Chemical potential of the reservoir.
+        T: (float) Temperature of the reservoir.
+    Returns:
+        hamiltonian_re: SparsePauliOp representing the real part of the Hamiltonian of the system.
+        hamiltonian_im: SparsePauliOp representing the imaginary part of the Hamiltonian of the system.
+    """
+    F = -((1- 2*F_L)+(1- 2*F_R))
+
+    hamiltonian_re = SparsePauliOp(["IZ", "ZI", "XY", "YX"], coeffs=[-eps / 2, eps / 2, -(gamma * (1 - 2*F)) / 4, -(gamma * (1 - 2*F)) / 4])
+    hamiltonian_im = -1 * SparsePauliOp(["XX", "YY", "II", "IZ", "ZI"], coeffs=[gamma / 4, -gamma / 4, -gamma / 2, (gamma * (1 - 2*F)) / 4, (gamma * (1 - 2*F)) / 4])
+    
+    return hamiltonian_re, hamiltonian_im
+
+def hamiltonian_generation_simple():
+    """
+    Generates a simple Hamiltonian for a single qubit system.
+
+    Returns:
+        hamiltonian_re: SparsePauliOp representing the Hamiltonian.
+    """
+    return SparsePauliOp(["IX", "XI"], coeffs=[1, -1])  # Example coefficients
+
+def statevector_to_densitymatrix(v):
+    """
+    Converts a Statevector to a density matrix.
+
+    Inputs:
+        v: (numpy.ndarray) The state vector to be converted.
+    
+    Returns:
+        (numpy.ndarray) The corresponding density matrix.
+    """
+    m = int(np.sqrt(len(v)))
+    return np.reshape(v, (m, m), order='F')
+
+
+def perform_vqte(ham_real, ham_imag, init_state,dt, nt, ansatz, init_param_values):
+    real_var_principle = RealMcLachlanPrinciple(qgt=ReverseQGT(), gradient=ReverseEstimatorGradient(derivative_type=DerivativeType.IMAG))
+    imag_var_principle = ImaginaryMcLachlanPrinciple(qgt=ReverseQGT(), gradient=ReverseEstimatorGradient())
+
+
+# Initialize lists to store results
+#second is the is expectation value of the number operator
+    trace_list = [1.0]
+    num_op_list = [np.trace(statevector_to_densitymatrix(init_state.data) @ np.array([[0, 0], [0, 1]])) / np.trace(statevector_to_densitymatrix(init_state.data))]
+    print("Initial expectation value of number operator using VQE:", num_op_list[0])
+
+
+
+    # Perform time evolution
+    for t in range(nt):
+        # Real evolution
+        evolution_problem = TimeEvolutionProblem(ham_real, dt/2)
+        var_qrte = VarQRTE(ansatz, init_param_values, real_var_principle, num_timesteps=1)
+        evolution_result_re = var_qrte.evolve(evolution_problem)
+        init_param_values = evolution_result_re.parameter_values[-1]
+        
+        # Imaginary evolution
+        evolution_problem = TimeEvolutionProblem(ham_imag, dt/2)
+        var_qite = VarQITE(ansatz, init_param_values, imag_var_principle, num_timesteps=1)
+        evolution_result_im = var_qite.evolve(evolution_problem)
+        init_param_values = evolution_result_im.parameter_values[-1]
+        
+        # Calculate the trace and expectation value of the number operator
+        trace = np.trace(statevector_to_densitymatrix(Statevector(ansatz.assign_parameters(init_param_values)).data))
+        trace_list.append(1.0) # Normalized so the trace is always 1
+        num_op_list.append(np.trace(statevector_to_densitymatrix(Statevector(ansatz.assign_parameters(init_param_values)).data) @ np.array([[0, 0], [0, 1]])) / trace)
+        #Stop
+    return num_op_list, trace_list
