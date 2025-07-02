6a3e96e72159fc1db29350d2cde29a27aa26fa01
sirpent5 <tessatonep@gmail.com>
2025-07-02 12:51:57 -0400

    Pretty sure I did it!! (Exact Diag)

--- singleQubitTwoReserviors/exactDiagMethods.py
+++ singleQubitTwoReserviors/exactDiagMethods.py
@@ -119,7 +119,10 @@ def output_exact_diag_results(exact_diag_results, time, nt, eps, mu_L,mu_R,T_L, 
     mu_effective = (mu_L + mu_R) / 2
     T_effective = (T_L + T_R) / 2
     time_axis = np.linspace(0, time, nt+1)
-    plt.plot(time_axis,steadyState, label='Steady State Expectation Value (Effective)', linestyle='solid', color='red')
+    plt.plot(time_axis, [steadyState] * (nt + 1),
+             label=f'Steady State ($\\langle n \\rangle$ = {steadyState:.4f})',
+             linestyle='--', color='red')
+    #plt.plot(time_axis,steadyState, label='Steady State Expectation Value (Effective)', linestyle='solid', color='red')
     plt.plot(time_axis, [1 / (1 + np.exp((eps - mu_effective) / T_effective))] * (nt + 1),label='Steady State Expectation Value (Effective)', linestyle='solid', color='red')
     #plt.plot(time_axis, [1 / (1 + np.exp((eps - mu) / T))] * (nt+1), label='Steady State Expectation Value', linestyle='solid')
     plt.plot(time_points, exact_diag_results, label='Expectation Value (Simulated)', marker='', linestyle='solid')
